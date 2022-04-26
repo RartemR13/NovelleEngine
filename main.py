c6dfa94a884830bb2011bbd20da85d6cd7db786b
NickolayYakovchukfm <creepertrase@gmail.com>
2022-04-27 01:27:41 +0300

    Slark, add some features to awesome brictleback

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 
-KV = """
-MDScreen:
-
-    FitImage:
-        source: 'pic.jpeg'
-
-    MDRaisedButton:
-        text: "Артем Хуесос"
-        pos_hint: {"center_x": .5, "center_y": .5}
-"""
-
 
 class MainApp(MDApp):
     def build(self):
-        self.root = Builder.load_string(KV)
+        self.root = Builder.load_file('main.kv')
 
 
 MainApp().run()
